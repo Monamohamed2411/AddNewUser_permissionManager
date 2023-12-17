bc49a0df5777e7a25bc8b7e474d4306a7b2a2108
unknown <you@example.com>
2023-12-17 16:08:28 +0200

    new project

---
+++ src/main/java/Pages/PageBase.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageBase {
+//protected WebDriver driver ;
+	
+	//craete constractor
+
+	public PageBase(WebDriver drive) {
+
+		PageFactory.initElements(drive, this);
+
+	}
+	
+	public static void clickButton(WebElement button) {
+
+		button.click();    
+	}
+	
+	public static void setTextElement(WebElement text, String value ) {
+
+		text.sendKeys(value);
+	}
+	
+}
+ 
